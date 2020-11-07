86b4067a5b9600dd791d3a940d0be26bdea028c5
Drew Bornt <andrew.bornt@gmail.com>
2020-11-06 19:38:16 -0600

    day 1

---
+++ BandNameGenerator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.\n")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What is the name of a pet of yours?\n")
+print("Your band name is " + city + " " + pet)
