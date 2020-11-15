91b7798890ee142afc9760f70a3ebd2754a6f8ed
Andrew Bornt <andrew.bornt@gmail.com>
2020-11-15 08:31:55 -0600

    finishing touches on the caesar cipher

--- CaesarCipher/main.py
+++ CaesarCipher/main.py
@@ -1,13 +1,46 @@
 import art
 
+def caesar(text, shift, direction):
+    toPrint = ""
+    if direction == "decode":
+        shift *= -1
+    for char in text:
+        if char in alphabet:
+            position = alphabet.index(char)
+        
+            newPosition = position + shift
+            newLetter = alphabet[newPosition]
+            toPrint += newLetter
+        else:
+            toPrint += char
+    
+    print(f"The {direction}d text is {toPrint}.")
+
 print(art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', \
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+toContinue = True
+while toContinue:
+
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 25
+    
+
+    caesar(text, shift, direction)
+
+    decision = input("Type 'yes' if you want to go again. Otherwise, type 'no'\n")
+    if decision == "no":
+        toContinue = False
+        print("Goodbye!")
+
+
+
 
 
 # def encrypt(text, shift):
@@ -27,24 +60,8 @@ shift = int(input("Type the shift number:\n"))
 #         newPosition = position - shift
 #         plainText += alphabet[newPosition]
 #     print(f"The decoded message is {plainText}")
-
-
-def caesar(text, shift, direction):
-    toPrint = ""
-    for letter in text:
-        position = alphabet.index(letter)
-        if direction == "decode":
-            shift *= -1
-        newPosition = position + shift
-        newLetter = alphabet[newPosition]
-        toPrint += newLetter
-    print(f"The {direction}d text is {toPrint}.")
-
-
 # if direction == "encode":
 #     encrypt(text, shift)
 
 # elif direction == "decode":
 #     decrypt(text, shift)
-
-caesar(text, shift, direction)
